bef19d4b9e713895d38fa43172f52135a08a4cbe
LunaGo77 <jdl137@pitt.edu>
2025-03-02 03:25:33 -0500

    done?

--- scam-shield/src/main.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TranscriptApp from "./TranscriptApp"; // Ensure this path is correct!
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <TranscriptApp /> 
-  </React.StrictMode>
-);
